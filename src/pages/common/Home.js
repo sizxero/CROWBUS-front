bf3c3adb1f5ade7aa6927c1e51c916438ce9a404
박유경 <pyk0514@kumoh.ac.kr>
2022-10-07 11:42:06 +0900

    [add] 메인 페이지 작성
    액터에 따라 메뉴 분류해야함

--- src/pages/common/Home.js
+++ src/pages/common/Home.js
@@ -1,8 +1,20 @@
+import { H4, ContainedButton } from "../../components/atoms";
+import { ColumnFlexBoxCenter } from  "../../components/molecules";
+import { MainLogo, Menu } from "../../components/organisms";
+
 const Home = () => {
     return (
-        <>
-        홈
-        </>
+        <ColumnFlexBoxCenter className="HomeContainer">
+            <MainLogo />
+            <ColumnFlexBoxCenter className="MenuWrapper">
+            <H4>김금오님, 환영합니다.</H4>
+            <Menu role="Passenger" />
+            <Menu role="Driver" />
+            <ContainedButton
+            className="containedBtnBigGray"
+            content="로그아웃" />
+            </ColumnFlexBoxCenter>
+        </ColumnFlexBoxCenter>
     );
 }
 
