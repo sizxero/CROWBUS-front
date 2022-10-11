1cf2fc50ed77e39b312a7195ad2e683b7bf908d2
박유경 <pyk0514@kumoh.ac.kr>
2022-10-11 12:39:28 +0900

    [chore] organisms component 폴더 이동

--- src/components/organisms/CommonWithData.js
+++ src/components/organisms/CommonWithData.js
@@ -8,8 +8,21 @@ const RouteSelectBox = (props) => {
         routes.map((rt) => processingData.push({name: rt.name+' '+rt.routeType, value: rt.id}))
 
     return (<><Select 
+        className={props.className}
         eventHandler={props.eventHandler} 
         label="노선" list={processingData} /></>);
 }
 
-export { RouteSelectBox }
+const BoardSelectBox = (props) => {
+    const boardlist = [
+        {name: '공지사항', value: '공지사항'},
+        {name: '분실물 게시판', value: '분실물게시판'},
+    ];
+    
+    return (<><Select 
+        className={props.className}
+        eventHandler={props.eventHandler} 
+        label="게시판" list={boardlist} /></>);
+}
+
+export { RouteSelectBox, BoardSelectBox }
--- src/components/organisms/index.js
+++ src/components/organisms/index.js
@@ -2,12 +2,12 @@ import { MainLogo, DriverLogo, TopLogo, Jumbotron, HeadingWithoutLink, HeadingWi
     Menu, DateSearch, TextSearch } from './Common';
 import { LoginShortcut } from './Login';
 import { MemberInfo, Ticket, TicketHistory, Drive } from './MyPage';
-import { Pagination, BoardButtons } from './Board';
+import { Pagination, BoardButtons, BoardWriteButtons } from './Board';
 import { SignUpButtons, SignUpInputTable } from './SignUp';
-import { RouteSelectBox } from './Reservation';
+import { RouteSelectBox, BoardSelectBox } from './CommonWithData';
 import { RouteTimetable } from './Timetable';
 
 export { MainLogo, DriverLogo, TopLogo, Menu, DateSearch, TextSearch, Jumbotron, 
     Pagination, BoardButtons, HeadingWithoutLink, HeadingWithLink, 
     MemberInfo, Ticket, TicketHistory, Drive, LoginShortcut,
-    SignUpButtons, SignUpInputTable, RouteSelectBox, RouteTimetable } 
+    SignUpButtons, SignUpInputTable, RouteSelectBox, RouteTimetable, BoardSelectBox, BoardWriteButtons } 
